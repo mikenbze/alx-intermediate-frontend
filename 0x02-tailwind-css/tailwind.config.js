b56754e5b71f3e203a5922c81b525c78c8ec2c7f
Michael Muriungi <50227674+mikenbze@users.noreply.github.com>
2025-07-03 02:28:35 +0300

    change
    Change

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./0x02-tailwind-css/1-index.html"
+  ],
+  safelist: [
+    "text-red-500",
+    "text-green-500",
+    "bg-blue-200",
+    "bg-blue-300",
+    "bg-blue-400",
+    "grid",
+    "grid-cols-3",
+    "gap-4",
+    "p-4",
+    "text-4xl"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
